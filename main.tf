441b16b7f0141e290dbb4d2415f63e1280e54105
Ahmad Nabil <ahmednmourad@gmail.com>
2024-03-04 01:48:28 +0200

    chore: Organize terraform project structure

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-central-1"
-}
-
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0a23a9827c6dab833"
   instance_type = "t2.micro"
